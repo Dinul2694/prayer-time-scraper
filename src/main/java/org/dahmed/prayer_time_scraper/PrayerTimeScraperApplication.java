ae7caa2079bf343f173c9b15f1200b2f59331d28
dinul.ahmed <dinulahmed@hotmail.co.uk>
2024-09-15 06:14:19 +0100

    Normalize line endings

---
+++ src/main/java/org/dahmed/prayer_time_scraper/PrayerTimeScraperApplication.java
@@ -0,0 +1,13 @@
+package org.dahmed.prayer_time_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrayerTimeScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrayerTimeScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dahmed/prayer_time_scraper/PrayerTimeScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dahmed.prayer_time_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrayerTimeScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
