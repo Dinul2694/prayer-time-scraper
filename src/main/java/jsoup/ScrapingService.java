ff06ee7de8de1e29da28b56255afea898f8df440
dinul.ahmed <dinulahmed@hotmail.co.uk>
2024-09-16 02:06:22 +0100

    remove unnecessary rows and isolate sunrise, jumuah rows

--- src/main/java/jsoup/ScrapingService.java
+++ src/main/java/jsoup/ScrapingService.java
@@ -4,32 +4,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
 public class ScrapingService {
-    public static void main(String[] args) throws IOException {
-        String url = "https://www.masjidbilal.uk/";
+    public static final String URL = "https://www.masjidbilal.uk/";
+    public static void main(String[] args) throws IllegalArgumentException {
+
 
         try {
 
-            // Select prayer table, assuming it is first table
-            var prayerTable = getPrayerTableFromMasjidBilal(Jsoup.connect(url).get());
+            // Select prayer table, assuming it is first table in tbody
+            var prayerTableHtml = getPrayerTableFromMasjidBilal(Jsoup.connect(URL).get());
 
-            if (prayerTable.isPresent()){
-                var prayerTableRows = prayerTable.get().select("tr");
+            if (prayerTableHtml.isPresent()){
+                var prayerTableBody = prayerTableHtml.get().select("tbody");
 
-                for(Element row: prayerTableRows){
-                    // print out row data
-                    Elements cols = row.select("th");
+                // removed first 2 rows as they were unnecessary
+                prayerTableBody.select("tr").remove(0);
+                prayerTableBody.select("tr").remove(0);
 
-                }
+                //segregated sunrise and jumuah rows (single element each)
+                var sunriseRow = prayerTableBody.select("tr").remove(1);
+                var jumuahRow = prayerTableBody.select("tr").remove(5);
+
+                // rows of standard prayers (elements)
+                var standardRows = prayerTableBody.select("tr");
+
+                log.info("standard rows: \n{}",standardRows);
+                log.info("sunrise row: \n{}",sunriseRow);
+                log.info("jumuah row: \n{}",jumuahRow);
 
             } else {
-                throw new Exception("Could not find any tables");
+                throw new IllegalArgumentException("Could not find any tables");
             }
 
 
