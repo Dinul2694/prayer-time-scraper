2b2378ede1975daa25e7dd03d49f74f2e93fa325
Dinul2694 <dinul_786@hotmail.com>
2024-09-19 00:36:45 +0100

    test commit

--- src/main/java/jsoup/ScrapingService.java
+++ src/main/java/jsoup/ScrapingService.java
@@ -35,6 +35,7 @@ public class ScrapingService {
                 log.info("standard rows: \n{}",standardPrayerRows);
                 log.info("sunrise row: \n{}",sunriseRow);
                 log.info("jumuah row: \n{}",jumuahRow);
+                log.info("testing");
 
             } else {
                 throw new IllegalArgumentException("Could not find any tables");
